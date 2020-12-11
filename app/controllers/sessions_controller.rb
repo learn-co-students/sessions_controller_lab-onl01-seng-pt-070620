836f3787a289907c9b7377a93c3bf94fa14ba4c1
Maddog164 <denise@henderson3.us>
2020-12-11 13:16:48 -0500

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+
+    def new
+    
+    end
+
+    def create
+        session[:username] = params[:username]
+        redirect_to '/'
+
+    end
+
+    def destroy
+        session.delete :username
+    end
+
+
+end
